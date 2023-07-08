970717adc8609c6bb09620c8e347c165911ec34e
Sathya Narayanan <sathya.n@beaconstac.com>
2023-07-09 00:18:20 +0530

    Added swagger for automatic API docs generation

--- web/zntrlhub_backend/urls.py
+++ web/zntrlhub_backend/urls.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Zntrlhub Backend API",
+        default_version='v1',
+        description="Welcome to Zntrlhub Backend! This API provides endpoints \
+            for interacting with the ZntrlHub backend.",
+        contact=openapi.Contact(email="nanasathya@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
