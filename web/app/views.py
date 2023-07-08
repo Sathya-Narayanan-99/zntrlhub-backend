21267750a49518ba8cf232845c592791fae025ea
Sathya Narayanan <sathya.n@beaconstac.com>
2023-07-08 21:46:17 +0530

    Implemented Custom User Model and JWT Authentication

--- web/app/views.py
+++ web/app/views.py
@@ -1,8 +1,11 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from rest_framework import permissions
+
 from .serializers import UserSerializer, GroupSerializer
 
+User = get_user_model()
 
 class UserViewSet(viewsets.ModelViewSet):
     """
--- web/zntrlhub_backend/urls.py
+++ web/zntrlhub_backend/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('app.urls'))
 ]
