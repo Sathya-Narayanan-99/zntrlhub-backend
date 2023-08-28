7d8811c6f2e164c143e250c5c47f099ea44ebdf5
Sathya Narayanan <sathya.n@beaconstac.com>
2023-08-29 01:54:23 +0530

    Fixed auth

--- app/backends.py
+++ app/backends.py
@@ -1,4 +1,8 @@
+from urllib.parse import urlparse
 from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from django.contrib.auth.models import AnonymousUser
+
 from app.tenant import set_current_account
 
 from app.models import Account
@@ -11,10 +15,14 @@ class CustomAuthentication(JWTAuthentication):
             user, jwt_value = authenticated_user
             set_current_account(user.account)
         else:
-            site = request.get_host()
+            site = request.META.get('HTTP_ORIGIN')
+            if site:
+                parsed_url = urlparse(site)
+                site = parsed_url.netloc.strip()
             try:
                 account = Account.objects.get(site=site)
                 set_current_account(account)
+                return AnonymousUser(), {}
             except Account.DoesNotExist:
                 return None
 
